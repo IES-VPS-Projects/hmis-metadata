79903046920d9b13ac85d25c490a86f6d8c85051
Ekirapapaul <ekirapapaul@gmail.com>
2022-01-16 22:21:42 +0300

    Add new diagnosis concepts

--- configuration/sql/diagnosis_map.sql
+++ configuration/sql/diagnosis_map.sql
@@ -5,3 +5,31 @@ CREATE TABLE openmrs.concept_map_505
     resource_type VARCHAR(50) NOT NULL,
     concept_id INTEGER
 );
+
+alter table disease_concepts_map
+	add concept_uuid varchar(50) null;
+
+alter table disease_concepts_map_b
+	add concept_uuid varchar(50) null;
+
+create table mapping_705a
+(
+	index_id int null,
+	name varchar(50) null,
+	child_uuid varchar(50) null,
+	child_name varchar(50) null,
+	child_id int auto_increment,
+	constraint mapping_705a_pk
+		primary key (child_id)
+);
+
+create table mapping_705b
+(
+	index_id int null,
+	name varchar(50) null,
+	child_uuid varchar(50) null,
+	child_name varchar(50) null,
+	child_id int auto_increment,
+	constraint mapping_705b_pk
+		primary key (child_id)
+);
